1f0269f4e1c0365940e229f8a3adc98ab90490a1
lnwza007 ⚡️ <gcalku@gmail.com>
2024-10-10 18:50:04 +0700

    เพิ่มการส่งค่าข้าม component

--- src/components/story.rs
+++ src/components/story.rs
@@ -1,29 +1,32 @@
+// story.rs
 #![allow(non_snake_case)]
 
 use dioxus::prelude::*;
 use crate::components::StoryCard;
 use crate::styles::story_style::STYLE;
 
+const _IMG: manganis::ImageAsset = manganis::mg!(image("./src/assets/img_4.jpg"));
+
 #[component]
 pub fn Story() -> Element {
     rsx! {
         style { {STYLE} }
         div { class: "note-container",
-            StoryCard {}
-            StoryCard {}
-            StoryCard {}
-            StoryCard {}
-            StoryCard {}
-            StoryCard {}
-            StoryCard {}
-            StoryCard {}
-            StoryCard {}
-            StoryCard {}
-            StoryCard {}
-            StoryCard {}
-            StoryCard {}
-            StoryCard {}
-            StoryCard {}
+            StoryCard { image: _IMG.to_string() }
+            StoryCard { image: _IMG.to_string() }
+            StoryCard { image: _IMG.to_string() }
+            StoryCard { image: _IMG.to_string() }
+            StoryCard { image: _IMG.to_string() }
+            StoryCard { image: _IMG.to_string() }
+            StoryCard { image: _IMG.to_string() }
+            StoryCard { image: _IMG.to_string() }
+            StoryCard { image: _IMG.to_string() }
+            StoryCard { image: _IMG.to_string() }
+            StoryCard { image: _IMG.to_string() }
+            StoryCard { image: _IMG.to_string() }
+            StoryCard { image: _IMG.to_string() }
+            StoryCard { image: _IMG.to_string() }
+            StoryCard { image: _IMG.to_string() }
         }
     }
 }
--- src/components/story_card.rs
+++ src/components/story_card.rs
@@ -1,22 +1,24 @@
+// story_card.rs
 #![allow(non_snake_case)]
 
 use dioxus::prelude::*;
 
-//const _IMG: &str = manganis::mg!(file("src/assets/img_4.jpg"));
 const _PLAY: &str = manganis::mg!(file("src/assets/play.svg"));
 const _FAV: &str = manganis::mg!(file("src/assets/fav.svg"));
 
-const _IMG: manganis::ImageAsset = manganis::mg!(image("./src/assets/img_5.jpg"));
-// const _PLAY: manganis::ImageAsset = manganis::mg!(image("./src/assets/play.svg"));
-// const _FAV: manganis::ImageAsset = manganis::mg!(image("s./rc/assets/fav.svg"));
+
+#[derive(PartialEq, Props, Clone)]
+pub struct StoryCardProps {
+    image: String,
+}
 
 #[component]
-pub fn StoryCard() -> Element {
+pub fn StoryCard(props: StoryCardProps) -> Element {
     rsx! {
         div { class: "note-box note-out",
             div {
                 img {
-                    src: "{_IMG}",
+                    src: "{props.image}",
                     alt: "Image",
                     max_width: "252px",
                     max_height: "256px",
